7a7972ed67cb71b7834241adb500c93e8f2ba159
FernanZXT-dev <celizfernando82@gmail.com>
2026-08-28 17:47:55 -0500

    Separar estilos compartidos, de caja y dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import "./App.css";
-import App from './App.jsx'
+import "./styles/caja.css";
+import "./styles/dashboard.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
